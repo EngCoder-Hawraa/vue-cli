5742cb82c1dd38c92b15313a0253f27d0a938225
Hawraa Arkan <h.a.j.engineer@gmail.com>
2024-12-16 12:27:42 +0300

    updated

---
+++ src/components/MyForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="my-fporm">
+    <form action="" @submit.prevent>
+      <div>
+        <label for="">First Name</label>
+        <input type="text" />
+      </div>
+      <hr />
+      <div>
+        <label for="">Last Name</label>
+        <input type="text" />
+      </div>
+      <hr />
+      <div>
+        <label for="">Age</label>
+        <input type="number" />
+      </div>
+      <hr />
+      <div>
+        <label for="">Email</label>
+        <input type="email" />
+      </div>
+      <hr />
+      <div class="actions">
+        <input type="submit" value="submit" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyForm",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/MyMain.vue
+++ src/views/MyMain.vue
@@ -1,55 +1,16 @@
 <template>
   <div class="my-main">
     <h2>this is my main page</h2>
-    <keep-alive>
-      <my-component :name="name" v-if="!isCompTwo">
-        <template #slotHeader>
-          <h3>This is slot Header One</h3>
-        </template>
-        <template #slotInputs>
-          <div>
-            <input type="text" placeholder="Enter Your name" />
-            <input type="password" placeholder="Enter Your Password" />
-          </div>
-        </template>
-        <template #slotActions>
-          <div>
-            <input type="submit" value="Submit" />
-          </div>
-        </template>
-      </my-component>
-    </keep-alive>
-    <keep-alive>
-      <my-component2 name="Hawraa" v-if="isCompTwo">
-        <template #slotHeader>
-          <h3>This is slot Header Two</h3>
-        </template>
-        <template #slotInputs>
-          <div>
-            <input type="text" placeholder="Enter Your name" />
-            <input type="password" placeholder="Enter Your Password" />
-          </div>
-        </template>
-        <template #slotActions>
-          <div>
-            <input type="submit" value="Submit" />
-          </div>
-        </template>
-      </my-component2>
-    </keep-alive>
-    <button @click="isCompTwo = !isCompTwo">Toggle Data</button>
+    <MyForm></MyForm>
   </div>
 </template>
 
 <script>
-import MyComponent from "@/components/MyComponent.vue";
-import MyComponent2 from "@/components/MyComponent.vue";
-
+import MyForm from "@/components/MyForm.vue";
 export default {
   name: "MyMain",
   components: {
-    MyComponent,
-    MyComponent2,
+    MyForm,
   },
   data() {
     return {
