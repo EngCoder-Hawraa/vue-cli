6e3d73916f93b3bdafd6f00ee790ff62e9182bda
Hawraa Arkan <h.a.j.engineer@gmail.com>
2024-12-18 13:51:17 +0300

    updated

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="about">
     <h2>This is about page</h2>
+    <li v-for="st in students" :key="st.id">
+      <p>Name: {{ st.name }}</p>
+      <p>Age: {{ st.age }}</p>
+    </li>
   </div>
 </template>
 
 <script>
+import stMixin from "@/mixins/stMixin";
+
 export default {
   name: "AboutPage",
   components: {},
+  mixins: [stMixin],
   data() {
-    return {};
-  },
+    return {
+      firstName: 'Hawraa',
+      lastName: 'Arkan',
+    }
+  }
 };
 </script>
 
