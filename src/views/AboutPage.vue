f54e6664aa6af24428344cb0764d73c48ee9366d
Hawraa Arkan <h.a.j.engineer@gmail.com>
2024-12-18 13:29:34 +0300

    updated

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -15,7 +15,19 @@ export default {
   name: "AboutPage",
   components: {},
   data() {
-    return {};
+    return {
+      students: [],
+    };
+  },
+  methods: {
+    async getStudents() {
+      await fetch("https://course-backend.onrender.com/")
+        .then((res) => res.json())
+        .then((data) => (this.students = data));
+    },
+  },
+  async mounted() {
+    await this.getStudents();
   },
 };
 </script>
