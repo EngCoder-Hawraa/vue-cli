0ab2c2f545b872a5127b7056e24e38d95035cf34
Hawraa Arkan <h.a.j.engineer@gmail.com>
2024-12-21 10:48:01 +0300

    updated

--- src/components/MyNav.vue
+++ src/components/MyNav.vue
@@ -5,8 +5,12 @@
     </div>
     <div class="links">
       <ul>
-        <li></li>
-        <li v-for="link in links" :key="link">{{ link }}</li>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
       </ul>
     </div>
     <div class="btns">
